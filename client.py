c32d5dc08d685bcd20f0ae33df2c02b86f465e74
preetmodh <preetmodh@gmail.com>
2022-06-29 12:11:37 +0530

    client options

--- client.py
+++ client.py
@@ -14,11 +14,31 @@ def main():
     pass
 
 def choose_option():
-    pass
+    print("Welcom to Remote shell")
+    input("What would to you like to do: ")
+    print("1. Send command")
+    print("2. Send file")
+    print("3. Receive file")
+    print("4. Exit")
+    option = input("Enter option: ")
+    if option == '1':
+        send_command()
+    elif option == '2':
+        send_file()
+    elif option == '3':
+        receive_file()
+    elif option == '4':
+        sock.close()
+        sys.exit()
+    else:
+        print("Invalid option")
+        choose_option()
+
 
 #send command to server
 def send_command():
     while(True):
+        print("Enter back to go back")
         command = input("Enter command: ")
         if command == 'back':
             choose_option()
@@ -33,36 +53,43 @@ def send_command():
 
 #send file to server
 def send_file():
+    print("Enter back to go back")
     # Getting file details.
-    file_name = input("File Name:")
-    file_size = os.path.getsize(file_name)
+    file_name = input("File to send:")
+    if os.path.isfile(file_name):
+    
+        file_size = os.path.getsize(file_name)
+        sock.send(str.encode(file_name))
+        sock.send(str.encode(str.format(file_size)))
+        # Opening file and sending data.
+        with open(file_name, "rb") as file:
+            c = 0
+            # Starting the time capture.
+            start_time = time.time()
 
-  
-    # Opening file and sending data.
-    with open(file_name, "rb") as file:
-        c = 0
-        # Starting the time capture.
-        start_time = time.time()
+            # Running loop while c != file_size.
+            print("Sending file...")
+            while c <= file_size:
+                data = file.read(1024)
+                if not (data):
+                    break
+                sock.sendall(data)
+                c += len(data)
 
-        # Running loop while c != file_size.
-        while c <= file_size:
-            data = file.read(1024)
-            if not (data):
-                break
-            sock.sendall(data)
-            c += len(data)
+            # Ending the time capture.
+            end_time = time.time()
 
-        # Ending the time capture.
-        end_time = time.time()
-
-    print("File Transfer Complete . Total time to transfer: ", end_time - start_time)
+        print("File Transfer Complete . Total time to transfer: ", end_time - start_time)
+    else:
+        print ("File not exist")
 
 
 
 #receive file from server
 def receive_file():
+    print("Enter back to go back")
     # Getting file details.
-    file_name = input("File Name:")
+    file_name = input("File to receive:")
     file_size = int(sock.recv(1024))
 
     # Opening and reading file.
@@ -70,7 +97,7 @@ def receive_file():
         c = 0
         # Starting the time capture.
         start_time = time.time()
-
+        print("Receiving file...")
         # Running the loop while file is recieved.
         while c <= int(file_size):
             data = sock.recv(1024)
@@ -82,7 +109,7 @@ def receive_file():
         # Ending the time capture.
         end_time = time.time()
 
-    print("File transfer Complete.Total time: ", end_time - start_time)
+    print("File received .Total time: ", end_time - start_time)
 
 
 
